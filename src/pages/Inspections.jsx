051665b8c40f30c46c0494d5366806618ce86017
hamza.farhat@glovoapp.com <hamza.farhat@glovoapp.com>
2025-08-04 20:22:27 +0000

    Bug fix

--- src/pages/Inspections.jsx
+++ src/pages/Inspections.jsx
@@ -153,6 +153,70 @@ export default function InspectionsDashboard() {
       <Box sx={{ p: 4, background: "#f7fafd", minHeight: "100vh" }}>
         {/* Dashboard content (filters, charts, etc.) */}
 
+        {/* Filters */}
+        <Grid container spacing={2} mb={4}>
+          <Grid item xs={12} sm={3}>
+            <LocalizationProvider dateAdapter={AdapterDateFns}>
+              <DatePicker label="Start Date" value={startDate} onChange={setStartDate} renderInput={(params) => <TextField fullWidth {...params} />} />
+            </LocalizationProvider>
+          </Grid>
+          <Grid item xs={12} sm={3}>
+            <LocalizationProvider dateAdapter={AdapterDateFns}>
+              <DatePicker label="End Date" value={endDate} onChange={setEndDate} renderInput={(params) => <TextField fullWidth {...params} />} />
+            </LocalizationProvider>
+          </Grid>
+          <Grid item xs={12} sm={2}>
+            <FormControl fullWidth>
+              <InputLabel>City</InputLabel>
+              <Select multiple value={cityFilter} onChange={(e) => setCityFilter(e.target.value)} input={<OutlinedInput label="City" />} renderValue={(selected) => <Stack direction="row" gap={1}>{selected.map((value) => <Chip key={value} label={value} />)}</Stack>}>
+                {[...new Set(inspections.map(i => i.city))].map((c) => <MenuItem key={c} value={c}>{c}</MenuItem>)}
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid item xs={12} sm={2}>
+            <FormControl fullWidth>
+              <InputLabel>Inspector</InputLabel>
+              <Select multiple value={inspectorFilter} onChange={(e) => setInspectorFilter(e.target.value)} input={<OutlinedInput label="Inspector" />} renderValue={(selected) => <Stack direction="row" gap={1}>{selected.map((value) => <Chip key={value} label={value} />)}</Stack>}>
+                {[...new Set(inspections.map(i => i.inspected_by))].map((i) => <MenuItem key={i} value={i}>{i}</MenuItem>)}
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid item xs={12} sm={2}>
+            <FormControl fullWidth>
+              <InputLabel>Rider</InputLabel>
+              <Select multiple value={riderFilter} onChange={(e) => setRiderFilter(e.target.value)} input={<OutlinedInput label="Rider" />} renderValue={(selected) => <Stack direction="row" gap={1}>{selected.map((value) => <Chip key={value} label={value} />)}</Stack>}>
+                {[...new Set(inspections.map(i => i.rider_id))].map((r) => <MenuItem key={r} value={r}>{r}</MenuItem>)}
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid item xs={12} sm={2}>
+            <Button onClick={handleExportCSV} variant="outlined" fullWidth startIcon={<SearchIcon />}>Export CSV</Button>
+          </Grid>
+        </Grid>
+
+        {/* Donut Charts */}
+        <Grid container spacing={2}>
+          {FIELDS_TO_CHART.map((field, index) => {
+            const data = getDonutData(field);
+            return (
+              <Grid item xs={12} sm={6} md={3} key={field}>
+                <Typography align="center" variant="subtitle1">{field.replace(/_/g, " ").toUpperCase()}</Typography>
+                <ResponsiveContainer width="100%" height={200}>
+                  <PieChart>
+                    <Pie data={data} dataKey="value" nameKey="name" outerRadius={60} label>
+                      {data.map((entry, i) => (
+                        <Cell key={`cell-${i}`} fill={COLORS[i % COLORS.length]} />
+                      ))}
+                    </Pie>
+                    <ReTooltip />
+                    <Legend />
+                  </PieChart>
+                </ResponsiveContainer>
+              </Grid>
+            );
+          })}
+        </Grid>
+
         {/* Inspection List */}
         <Box mt={6}>
           <Typography variant="h5" mb={2}>Inspection Records</Typography>
