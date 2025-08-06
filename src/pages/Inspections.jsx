c97a38a352cf943a65d76ee9dceee56478dce36a
hamza.farhat@glovoapp.com <hamza.farhat@glovoapp.com>
2025-08-06 17:28:26 +0000

    test 2

--- src/pages/Inspections.jsx
+++ src/pages/Inspections.jsx
@@ -218,7 +218,7 @@ const handleSaveEdit = async () => {
                   {field.replace(/_/g, " ").toUpperCase()}
                 </Typography>
                 <Box sx={{ width: 200, height: 200 }}>
-                  <PieChart width={300} height={300}>
+                  <PieChart width={400} height={400}>
                     <Pie
                       data={data}
                       dataKey="value"
