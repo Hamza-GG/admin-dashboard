2f5ddc6b584e408e9a1397e7ed682c373b81c4da
hamza.farhat@glovoapp.com <hamza.farhat@glovoapp.com>
2025-08-08 10:56:21 +0000

    test loc functionin the navbar

--- src/.ipynb_checkpoints/App-checkpoint.jsx
+++ src/.ipynb_checkpoints/App-checkpoint.jsx
@@ -13,54 +13,9 @@ import ResetPassword from "./pages/ResetPassword";
 import VerifyEmail from "./pages/VerifyEmail";
 import Users from "./pages/Users";
 const theme = createTheme();
-/*
-// ✅ Location tracking component
-import authAxios from "../utils/authAxios";
 
-function LocationTracker() {
-  useEffect(() => {
-    const sendLocation = async () => {
-      console.log("📡 Trying to get location...");
 
-      if ("geolocation" in navigator) {
-        navigator.geolocation.getCurrentPosition(
-          async (position) => {
-            const { latitude, longitude } = position.coords;
-            console.log("📍 Got location:", latitude, longitude);
 
-            try {
-              const res = await authAxios.post(
-                "https://employee-inspection-backend.onrender.com/api/locations",
-                { latitude, longitude }
-              );
-
-              console.log("✅ Location sent, status:", res.status, res.data);
-            } catch (error) {
-              console.error("❌ Failed to send location:", error);
-            }
-          },
-          (error) => {
-            console.warn("⚠️ Geolocation error:", error);
-          },
-          { enableHighAccuracy: true }
-        );
-      } else {
-        console.warn("❌ Geolocation not supported");
-      }
-    };
-
-    sendLocation(); // Send once on load
-    const intervalId = setInterval(sendLocation, 60_000); // Every 1 minute
-
-    return () => clearInterval(intervalId);
-  }, []);
-
-  return null;
-}
-
-export default LocationTracker;
-
-*/
 
 // ✅ Protected route wrapper
 function ProtectedRoute({ children }) {
